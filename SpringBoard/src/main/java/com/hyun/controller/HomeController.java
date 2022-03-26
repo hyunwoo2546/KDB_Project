bbb7054d2d1bf4f5e666b54d1b99aa5d45cb724f
Hyunwoo <hyunwoo2546@naver.com>
2022-03-26 20:22:07 +0900

    3/26 second

--- SpringBoard/src/main/java/com/hyun/controller/HomeController.java
+++ SpringBoard/src/main/java/com/hyun/controller/HomeController.java
@@ -3,13 +3,20 @@ package com.hyun.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +25,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,6 +44,47 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	/* # 회원가입 페이지 GET 맵핑 */
+	@RequestMapping(value = "/member/join", method = RequestMethod.GET)
+	public void join() {
+	}
+	
+	/* # 이메일 인증 */
+	@ResponseBody
+	@RequestMapping(value = "/emailAuth", method = RequestMethod.POST)
+	public String emailAuth(String email) {		
+		Random random = new Random();
+		int checkNum = random.nextInt(888888) + 111111;
+
+		/* 이메일 보내기 */
+        String setFrom = "자신의 이메일을 입력해주세요";
+        String toMail = email;
+        String title = "회원가입 인증 이메일 입니다.";
+        String content = 
+                "홈페이지를 방문해주셔서 감사합니다." +
+                "<br><br>" + 
+                "인증 번호는 " + checkNum + "입니다." + 
+                "<br>" + 
+                "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+        
+        try {
+            
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(setFrom);
+            helper.setTo(toMail);
+            helper.setSubject(title);
+            helper.setText(content,true);
+            mailSender.send(message);
+            
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        return Integer.toString(checkNum);
+ 
 	}
 	
 }
---
+++ SpringBoard/src/main/java/lotto/Lotto.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+		
+		System.out.println("로또 번호 추출 갯수 : ");
+		
+		Scanner sc = new Scanner(System.in);
+		int userNum = sc.nextInt();
+		
+		for(int i = 0; i <= userNum; i++) {
+			System.out.println(i + " 번째 : " + lottoNumbers());
+		}
+		sc.close();
+	}
+	
+	static String lottoNumbers() {
+		Set<Integer>set = new HashSet<Integer>();
+		
+		while (set.size() != 6) {
+			set.add((int)(Math.random() * 45 + 1));
+		}
+		
+		List<Integer> list = new ArrayList<Integer>(set);
+		Collections.sort(list);
+		
+		return list.toString();
+	}
+	
+	
+}
